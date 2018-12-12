22c5c33ea37b4050c65d8b41ac64718af8cb1f87
bsomana <bsomana@DIN80011422.corp.capgemini.com>
2018-12-12 15:49:00 +0530

    change1

--- helllworld/src/cg/com/hellllworld.java
+++ helllworld/src/cg/com/hellllworld.java
@@ -2,9 +2,9 @@ package cg.com;
 
 public class hellllworld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("bharathi somana");
 	}
 
 }
